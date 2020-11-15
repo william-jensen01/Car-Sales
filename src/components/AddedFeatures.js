f72be5aa32d4f4b05c780e26db6269b8c223c31d
William Jensen <williambjensen01@gmail.com>
2020-11-15 11:13:21 -0700

    MVP

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import AddedFeature from './AddedFeature';
 
@@ -8,9 +9,9 @@ const AddedFeatures = props => {
       <h6>Added features:</h6>
       {props.car.features.length ? (
         <ol type="1">
-          {props.car.features.map(item => (
-            <AddedFeature key={item.id} feature={item} />
-          ))}
+          {props.car.features.map(a => {
+            return(<AddedFeature feature={a} key={a.id} />)
+          })}
         </ol>
       ) : (
         <p>You can purchase items from the store.</p>
@@ -19,4 +20,10 @@ const AddedFeatures = props => {
   );
 };
 
-export default AddedFeatures;
+const mapStateToProps = state => {
+  return {
+    car: state.car,
+    additionalPrice: state.additionalPrice
+  }
+}
+export default connect(mapStateToProps, {})(AddedFeatures)
